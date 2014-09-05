ffed940336c37351122963f9065897168397fe99
Isaac Hess <isaachess@gmail.com>
2014-09-05 16:58:20 -0600

    Cleaned iamge

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ var express = require('express')
 var expressConductor = require('express-conductor')
 var app = express()
 var bodyParser = require('body-parser')
-var knox = require('knox')
-var client = knox.createClient({
-    key: process.env.AWS_ACCESS_KEY_ID,
-    secret: process.env.AWS_SECRET_ACCESS_KEY,
-    bucket: 'wolfpackplay'
-})
-
 var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017'
 var mongoose = require('mongoose')
 mongoose.connect(MONGO_URL)
