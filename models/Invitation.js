87be5395a30075dd3d44d392b1dea3b0747c8ad7
taterbase <shankga@gmail.com>
2014-09-06 15:22:37 -0600

    send invites over twilio

--- models/Invitation.js
+++ models/Invitation.js
@@ -1,4 +1,9 @@
+var async = require('async')
 var mongoose = require('mongoose')
+var User = require('./User')
+var Message = require('./Message')
+var PlayDate = require('./PlayDate')
+var Child = require('./Child')
 var Schema = mongoose.Schema
 
 var InvitationSchema = new Schema({
@@ -21,6 +26,38 @@ var InvitationSchema = new Schema({
     deleted: {
         type: Boolean
     }
+})
+
+InvitationSchema.pre('save', function(next) {
+    var invite = this
+
+    async.parallel({
+        playdate: function(cb) {
+            PlayDate.findOne({_id: invite.playDateId}, cb)
+        },
+
+        parent: function(cb) {
+            Child.findOne({_id: invite.childId}).exec()
+            .then(function(child) {
+                User.findOne({_id: child.parentId}, cb)
+            }, cb)
+        }
+    }, function(err, results) {
+        if (err)
+            return next(err)
+
+        var playdate = results.playdate
+        var parent = results.parent
+
+        if (parent._id == playdate.ownerId)
+            return next()
+
+        User.findOne({_id: playdate.ownerId}).exec()
+        .then(function(owner) {
+            Message.sendMessage(parent.phone, owner.phone, "You've been invited to a Wolfpack playdate! http://wolfpackplay.co", next)
+        }, next)
+
+    })
 })
 
 module.exports = mongoose.model('Invitation', InvitationSchema)
