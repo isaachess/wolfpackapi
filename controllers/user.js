d0f51018eaa8e8d708ce153d0fe83713ea5f7aa9
taterbase <shankga@gmail.com>
2014-09-05 22:52:56 -0600

    some friend routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,17 @@
 var multiparty = require('connect-multiparty')
+var async = require('async')
 var s3 = require('../lib/s3')
 var response = require('../lib/serverResponse')
 var User = require('../models/User')
 
 module.exports.init = function(app) {
+
+    app.get('/users', function(req, res) {
+        User.find({}).exec()
+        .then(function (users) {
+            return res.json(users)
+        }, response.serverError(res))
+    })
 
     app.post('/users', function(req, res) {
         User.create(req.body)
@@ -33,11 +41,38 @@ module.exports.init = function(app) {
 
     })
 
-    app.get('/users', function(req, res) {
-        User.find({}).exec()
-        .then(function (users) {
-            return res.json(users)
-        }, response.serverError(res))
+    app.post('/users/:userId/friends', function(req, res) {
+        User.update({_id: req.params.userId}, {
+            $addToSet: {
+                frends: {
+                    $each: req.params.friendIds
+                }
+            }
+        }, function(err) {
+            if (err) {
+                console.log(err)
+                return res.json({ok: false}, 500)
+            }
+
+            res.json({ok: true})
+        })
+    })
+
+    app.get('/users/:userId/friends', function(req, res) {
+        var numbers = req.query.numbers
+
+        async.map(numbers, function(number, cb) {
+            User.findOne({phone: number}, function(err, user) {
+                cb(err, user)
+            })
+        }, function(err, users) {
+            if (err) {
+                console.log(err)
+                return res.json({ok: true})
+            }
+
+            res.json(users.filter(function(user) { return !!user}))
+        })
     })
 
 }
--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema
 var UserSchema = new Schema({
     phone: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
 
     imageUrl: String,
